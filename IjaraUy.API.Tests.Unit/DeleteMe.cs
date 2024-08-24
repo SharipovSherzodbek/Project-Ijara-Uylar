6edc0bfd7a5a5cf6fba737a283b732a3b7d0f762
Sherzodbek Sharipov <sherzodbekdeveloper@gmail.com>
2024-08-24 22:47:47 +0500

    INFRA: Initialize Unit Test Project

---
+++ IjaraUy.API.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,17 @@
+﻿//=================================================
+// Copyright(c) Coalition of Good-Hearted Engineers
+// Free To Use For Your Comfort and Peace !
+//=================================================;
+using Xunit;
+
+namespace IjaraUy.API.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+
+
+
+    }
+}
