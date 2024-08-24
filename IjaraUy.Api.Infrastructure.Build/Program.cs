ea178db4ab4f12332836a9f2c479c964d95cc5ae
Sherzodbek Sharipov <sherzodbekdeveloper@gmail.com>
2024-08-25 00:31:29 +0500

    INFRA: Initiazlie Build Project

---
+++ IjaraUy.Api.Infrastructure.Build/Program.cs
@@ -0,0 +1,84 @@
+﻿//=================================================
+// Copyright(c) Coalition of Good-Hearted Engineers
+// Free To Use For Your Comfort and Peace !
+//=================================================
+
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+
+var aDotNetClient = new ADotNetClient();
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "IjaraUy Build Pipeline",
+
+    OnEvents = new Events
+    {
+        Push = new PushEvent
+        {
+            Branches = new string[] { "main" }
+        },
+
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "main" }
+        }
+    },
+
+    Jobs = new Dictionary<string, Job>
+    {
+        {
+            "build",
+            new Job
+            {
+                RunsOn = BuildMachines.Windows2022,
+
+                Steps = new List<GithubTask>
+                {
+                    new CheckoutTaskV2
+                    {
+                        Name = "Check out"
+                    },
+
+                    new SetupDotNetTaskV1
+                    {
+                        Name = "Setup .Net",
+
+                        TargetDotNetVersion = new TargetDotNetVersion
+                        {
+                            DotNetVersion = "8.0.204",
+                            IncludePrerelease = true
+                        }
+                    },
+
+                    new RestoreTask
+                    {
+                        Name = "Restore"
+                    },
+
+                    new DotNetBuildTask
+                    {
+                        Name = "Build"
+                    },
+
+                    new TestTask
+                    {
+                        Name = "Test"
+                    }
+                }
+            }
+        }
+    }
+};
+
+string buildScriptPath = "../../../../.github/workflows/dotnet.yml";
+string directoryPath = Path.GetDirectoryName(buildScriptPath);
+
+if (!Directory.Exists(directoryPath))
+{
+    Directory.CreateDirectory(directoryPath);
+}
+
+aDotNetClient.SerializeAndWriteToFile(githubPipeline, path: buildScriptPath);
